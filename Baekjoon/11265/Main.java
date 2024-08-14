8fe447887bb458a5f5ae854cd8000162d85c7f1e
송하은 <she3469@gmail.com>
2024-08-14 10:27:52 +0900

    240814 끝나지 않는 파티

---
+++ Baekjoon/11265/Main.java
@@ -0,0 +1,41 @@
+// 끝나지 않는 파티
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] parties = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				parties[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					int distance = parties[i][k] + parties[k][j];
+					if (distance <= parties[i][j]) {
+						parties[i][j] = distance;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken()) - 1;
+			int B = Integer.parseInt(st.nextToken()) - 1;
+			int C = Integer.parseInt(st.nextToken());
+
+			System.out.println(parties[A][B] <= C ? "Enjoy other party" : "Stay here");
+		}
+
+	}
+}
